9ce83f25e083c5e8dc5ace2115d148a9739fde23
DimK19 <kyriakidis_dimitrios@outlook.com>
2021-05-23 19:04:50 +0300

    Add entity verifications, change portal to 5000

--- src/answer/dto/create-answer.dto.ts
+++ src/answer/dto/create-answer.dto.ts
@@ -1,8 +1,12 @@
+import { IsString, IsNotEmpty } from 'class-validator';
 import { Question } from '../../question/entities/question.entity';
 import { User } from '../../user/entities/user.entity';
 
 export class CreateAnswerDto {
+    @IsString()
+    @IsNotEmpty()
     readonly text: string;
+    
     readonly positiveVotes: number;
     readonly negativeVotes: number;
     readonly question: Question;
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,7 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
     readonly username: string;
 }
